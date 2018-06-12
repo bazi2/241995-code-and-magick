625cf5133aee76122421914d23bfabd374229d51
Tatyana Kudryavtseva <bazi2@rambler.ru>
2018-06-12 13:40:39 +0300

    Прошу помощь наставника по ДЗ

--- js/stat.js
+++ js/stat.js
@@ -10,6 +10,7 @@ var FONT_GAP = 16;
 var GIST_HEIGHT = 150;
 var COLUMN_WIDTH = 40;
 var COLUMN_GAP = 50;
+var barWidth = COLUMN_WIDTH + COLUMN_GAP;
 
 //Убираю повторения отрисовки облака задав функцию
 var renderCloud = function(ctx, x, y, color) {
@@ -32,27 +33,42 @@ window.renderStatistics = function (ctx) {
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + (GAP * 4));
   ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + (GAP * 6));
 
+  //Сокращаю запись имен через переменные
+  var playerIndex = 0;
+  var playerName = 'Вы';
+
+  var players = ['Вы', 'Кекс', 'Катя', 'Игорь'];
+
   //Данные игрока: 'Вы'
   ctx.fillStyle = 'black';
-  ctx.fillText('Вы', CLOUD_X + GAP, CLOUD_HEIGHT);
+  ctx.fillText(playerName, CLOUD_X + GAP, CLOUD_HEIGHT);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(CLOUD_X + GAP, CLOUD_Y + (GAP * 8), COLUMN_WIDTH, GIST_HEIGHT);
+  ctx.fillRect(CLOUD_X + (GAP * 1) + (barWidth * playerIndex), CLOUD_Y + (GAP * 8), COLUMN_WIDTH, GIST_HEIGHT);
 
   //Данные игрока: 'Кекс'
+  var playerIndex = 1;
+  var playerName = 'Кекс';
+
   ctx.fillStyle = 'black';
-  ctx.fillText('Кекс', CLOUD_X + COLUMN_WIDTH + COLUMN_GAP, CLOUD_HEIGHT);
+  ctx.fillText(playerName, CLOUD_X + barWidth, CLOUD_HEIGHT);
   ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-  ctx.fillRect(CLOUD_X + COLUMN_WIDTH + COLUMN_GAP, COLUMN_WIDTH + COLUMN_GAP, COLUMN_WIDTH, GIST_HEIGHT);
+  ctx.fillRect(CLOUD_X + (GAP * 0) + (barWidth * playerIndex), barWidth, COLUMN_WIDTH, GIST_HEIGHT);
 
   //Данные игрока: 'Катя'
+  var playerIndex = 2;
+  var playerName = 'Катя';
+
   ctx.fillStyle = 'black';
-  ctx.fillText('Катя', CLOUD_X + (COLUMN_WIDTH + COLUMN_GAP) * 2, CLOUD_HEIGHT);
+  ctx.fillText(playerName, CLOUD_X + barWidth * 2, CLOUD_HEIGHT);
   ctx.fillStyle = 'rgba(0, 0, 255, 0.7)';
-  ctx.fillRect(CLOUD_X + (COLUMN_WIDTH + COLUMN_GAP) * 2, COLUMN_WIDTH + COLUMN_GAP, COLUMN_WIDTH, GIST_HEIGHT);
+  ctx.fillRect(CLOUD_X + (GAP * 0) + (barWidth * playerIndex), barWidth, COLUMN_WIDTH, GIST_HEIGHT);
 
   //Данные игрока: 'Игорь'
+  var playerIndex = 3;
+  var playerName = 'Игорь';
+
   ctx.fillStyle = 'black';
-  ctx.fillText('Игорь', CLOUD_X + (COLUMN_WIDTH + COLUMN_GAP) * 3, CLOUD_HEIGHT);
+  ctx.fillText(playerName, CLOUD_X + (barWidth * 3), CLOUD_HEIGHT);
   ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
-  ctx.fillRect(CLOUD_X + (COLUMN_WIDTH + COLUMN_GAP) * 3, COLUMN_WIDTH + COLUMN_GAP, COLUMN_WIDTH, GIST_HEIGHT);
+  ctx.fillRect(CLOUD_X + (GAP * 0) + (barWidth * playerIndex), barWidth, COLUMN_WIDTH, GIST_HEIGHT);
 };
